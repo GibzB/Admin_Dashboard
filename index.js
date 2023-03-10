25df5ecb11de6e7189f4f780ac0378c3113dee73
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-10 20:12:55 +0300

    updated

---
+++ index.js
@@ -0,0 +1,6 @@
+// space, scary, military, romantic, cowboy, fantasy, superhero 
+favouriteMovieGenre ("cowboy") 
+// watermelon, tomato, banana, orange, avocado, blueberry 
+favouriteFruit("banana") 
+// light, dark favouriteMode("dark") // sharp, soft, round 
+favouriteEdgeStyle("sharp")
